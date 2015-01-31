e08298fcf9e8735a64e2a189afa3714135fccfff
Ben Rothman <bensrothman@gmail.com>
2015-01-30 23:29:07 -0600

    work on base building. mostly add new image assets and very basic game routing

---
+++ Assets/Base/Center.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Center : MonoBehaviour {
+	private float health;
+
+	void GameStart () {
+		health = 100f;
+	}
+
+	// Use this for initialization
+	void Start () {
+		GameEventManager.GameStart += GameStart;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0f) {
+			GameEventManager.TriggerGameOver();
+		}
+	}
+}
---
+++ Assets/Managers/GUIManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class GUIManager : MonoBehaviour {
+	private static GUIManager instance;
+	public GUIText gameOverText, instructionsText, titleText;
+	public GUITexture HUD;
+	
+	void Start () {
+		instance = this;
+		GameEventManager.GameStart += GameStart;
+		GameEventManager.GameOver += GameOver;
+		gameOverText.enabled = false;
+		HUD.enabled = false;
+	}
+
+	private void GameOver () {
+		gameOverText.enabled = true;
+		instructionsText.enabled = true;
+		HUD.enabled = false;
+		enabled = true;
+	}
+	
+	void Update () {
+		if(Input.GetButtonDown("Jump")){
+			GameEventManager.TriggerGameStart();
+		}
+	}
+	
+	private void GameStart () {
+		gameOverText.enabled = false;
+		instructionsText.enabled = false;
+		titleText.enabled = false;
+		HUD.enabled = true;
+		enabled = false;
+	}
+}
